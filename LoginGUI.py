b2bfc0593b8206aba08fdba35414a69ea0d14518
Samuel <Samuelyos98@hotmail.com>
2022-05-09 09:57:59 +0200

    test git

---
+++ LoginGUI.py
@@ -0,0 +1,64 @@
+from PyQt6 import QtWidgets, uic
+from AdminRequestGUI import AdminRequestGUI
+from TeacherRequestGUI import TeacherRequestGUI
+from login_class import Login
+from courseleader_class import Courseleader
+from admin_class import Admin
+
+##
+hugo = Courseleader('Hugo', 'Test', 'abc123')
+hugo_login = Login(hugo, 'password')
+
+henrik = Admin('Henrik', 'Hansen', 'zyx999')
+henrik_login = Login(henrik, 'password')
+##
+
+class LoginGUI(QtWidgets.QDialog):
+    """Klasse for selve ændring af lektioner GUI"""
+
+    def __init__(self):
+        super(LoginGUI, self).__init__()
+        uic.loadUi('GUI/LoginGUI.ui', self)
+
+        self.ExitPush.clicked.connect(self.Exit_button_pressed)
+        self.LoginPush.clicked.connect(self.Login_button_pressed)
+        self.currentUser = None
+        self.show()
+
+    def Login_button_pressed(self):
+
+        self.written_username = self.usernameLabel.text()
+        self.written_password = self.passwordLabel.text()
+
+        if len(self.written_username) == 6:
+            for user in Login.all_users:
+                if self.written_username == user.get_userID() and self.written_password == user.get_password():
+                    self.currentUser = user
+
+                    # Hvis brugeren er administrator, åbnes tilhørende GUI
+                    if self.currentUser.get_role() == 'admin':
+                        self.close()
+                        self.admingui = AdminRequestGUI()
+                        self.admingui.show()
+
+                    # Hvis brugeren er underviser, åbnes tilhørende GUI
+                    elif self.currentUser.get_role() == 'courseleader':
+                        self.close()
+                        self.courseleadergui = TeacherRequestGUI()
+                        self.courseleadergui.show()
+                else:
+                    continue
+        else:
+            #Følgende er tekst i en tekstbox som ændre sig ved hvert forkert login-forsøg for at give feedback
+            if self.wrongLabel.text() == '':
+                self.wrongLabel.setText('Wrong username or password!\nTry again')
+            elif len(self.wrongLabel.text()) >= 50:
+                self.wrongLabel.setText('Wrong username or password!\nTry again')
+            else:
+                temp_label = self.wrongLabel.text()
+                temp_label = temp_label + '...'
+                self.wrongLabel.setText(temp_label)
+
+
+    def Exit_button_pressed(self):
+        self.close()
---
+++ courseleader_class.py
@@ -0,0 +1,37 @@
+class Courseleader:
+    """Info om ansette kursusledere"""
+    def __init__(self, name, lastname, employeeID):
+        self.__name = name
+        self.__lastname = lastname
+        self.__employeeID = employeeID
+        self.role = "courseleader"
+        self.initials = f"{self.__name[0]}{self.__lastname[0]}"
+        self.courselist = []
+
+    def set_name(self, new_name): self.__name = new_name
+
+    def get_name(self): return self.__name
+
+    def set_lastname(self, new_lastname): self.__lastname = new_lastname
+
+    def get_lastname(self): return self.__lastname
+
+    def set_employeeID(self, new_employeeID): self.__employeeID = new_employeeID
+
+    def get_employeeID(self): return self.__employeeID
+
+    # metoder til at fjerne eller tilføje kurser til kursuslederens liste over kurser
+
+    def add_course(self, new_course):
+        self.courselist.append(new_course)
+        return print(f'{new_course} is added to courses for {self.__name}')
+
+    def remove_course(self, old_course):
+        if old_course in self.courselist:
+            self.courselist.pop(old_course)
+            print(f'{old_course} is removed')
+        else:
+            print(f'Course is not in list')
+
+    def __str__(self):
+        return f'{self.initials}: {self.get_name()} {self.get_lastname()}'
